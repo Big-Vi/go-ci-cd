be6e6b1954ca3737f23ce680162f9dbd16521f28
big-vi <vignesh.murugan@hotmail.com>
2022-07-11 18:44:02 +1200

    creating record in postgres and output as json

--- main.go
+++ main.go
@@ -3,18 +3,16 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"os"
+	"encoding/json"
+
 
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "This is home page.")
-}
 
 func main() {
 	db, err := initDB()
@@ -23,7 +21,7 @@ func main() {
 	}
 	defer db.Conn.Close()
 
-	http.HandleFunc("/", homePage)
+	http.HandleFunc("/", homePage(db.Conn))
 	log.Println("Server is running...")
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
@@ -42,6 +40,7 @@ func initDB() (Database, error) {
 		log.Fatalf("Error loading .env file")
 	}
 
+	// Creating connection string for DB
 	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		os.Getenv("POSTGRES_HOST"),
 		os.Getenv("POSTGRES_PORT"),
@@ -56,15 +55,46 @@ func initDB() (Database, error) {
 		return db, err
 	}
 
-	defer conn.Close()
-
 	db.Conn = conn
 
 	if _, err := db.Conn.Exec("CREATE TABLE IF NOT EXISTS persons (PersonID int PRIMARY KEY,LastName varchar(255))"); err != nil {
 		fmt.Println(err)
 	}
 
-	rows, err := db.Conn.Query("SELECT COUNT(*) FROM persons")
+	if _, err := db.Conn.Exec("INSERT INTO persons(PersonID, LastName) VALUES($1, $2) ON CONFLICT (PersonID) DO NOTHING", 1, "murugan"); err != nil {
+		fmt.Println(err)
+	}
+
+	log.Println("DB connected")
+	return db, nil
+}
+
+func homePage(conn *sql.DB) (http.HandlerFunc) {
+	return func (w http.ResponseWriter, r *http.Request) {
+		record, err := countRecords(conn)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Records count:", record)
+
+		type Record struct{
+			Row int `json:row`
+		}
+		recordJSON := Record{Row: record}
+		
+		byteArray, err := json.Marshal(recordJSON)
+		if err != nil {
+			fmt.Print(err)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(string(byteArray)))
+	}
+}
+
+func countRecords(conn *sql.DB) (int, error) {
+	rows, err := conn.Query("SELECT COUNT(*) FROM persons")
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -78,10 +108,6 @@ func initDB() (Database, error) {
 		}
 		rows.Close()
 	} 
-	fmt.Println(count)
-
-	log.Println("DB connected")
-	return db, nil
+	return count, nil
 }
-
 
